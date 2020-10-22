fa79839fa06bc4971cf89c275d5cf72ba6985595
pritamSarkar123 <pritamsarkar84208220@gmail.com>
2020-10-22 20:51:19 +0530

    branch one method added

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 def masterBranchFunction():
     print("This is master branch function")
 
-
+def branchOneFunction():
+    print("This is branch One function")
 
 if __name__=="__main__":
     masterBranchFunction()
